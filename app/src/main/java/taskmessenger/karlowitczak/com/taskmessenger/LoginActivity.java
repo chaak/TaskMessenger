1d053fa7c9c6ba40a2d89fa0c8a35e4de3da6460
JakubWitczak <jakubwitczak6@gmail.com>
2017-03-27 16:10:05 +0200

    added firebase configuration, internet permission and template for login and registration

---
+++ app/src/main/java/taskmessenger/karlowitczak/com/taskmessenger/LoginActivity.java
@@ -0,0 +1,27 @@
+package taskmessenger.karlowitczak.com.taskmessenger;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button registerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        registerButton = (Button)findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/taskmessenger/karlowitczak/com/taskmessenger/RegisterActivity.java
+++ app/src/main/java/taskmessenger/karlowitczak/com/taskmessenger/RegisterActivity.java
@@ -3,11 +3,11 @@ package taskmessenger.karlowitczak.com.taskmessenger;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StartActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_register);
     }
 }
